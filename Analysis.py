aa8563eaddfd112af623a428fee869aa6d04b66a
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2023-11-03 01:47:50 +0800

    enhancement: tidy up code, improve logic to be more easily extendible

--- Analysis.py
+++ Analysis.py
@@ -1,426 +1,61 @@
 from HTTP import Get
 from HTMLParser import HTMLParser
 from enum import Enum
+from abc import ABC, abstractmethod
 
-class Analysis():
-    def __init__(self, input):
-        self.input = self.parseInput(input)
-
-        self.analysisJobRole = {JobRole.SOFTWARE_ENGINEER: 0, JobRole.QUALITY_ASSURANCE: 0, JobRole.DATA_SCIENTIST: 0, JobRole.WEB_DEVELOPER: 0, JobRole.FRONT_END: 0, JobRole.BACK_END: 0, JobRole.FULLSTACK: 0, JobRole.RESEARCH: 0, JobRole.NETWORKING: 0, JobRole.TEST_ENGINEER: 0, JobRole.ML_ENGINEER: 0, JobRole.AI_ENGINEER: 0}
-        self.analysisRequiredDegree = {RequiredDegree.NONE_REQUIRED: 0, RequiredDegree.BACHELOR:0, RequiredDegree.GRADUATE:0, RequiredDegree.MASTER:0, RequiredDegree.PHD:0}
-        self.analysisTechSkills = {TechSkill.JAVA: 0, TechSkill.JAVASCRIPT: 0, TechSkill.CPP: 0, TechSkill.PYTHON: 0, TechSkill.SQL: 0, TechSkill.RUBY: 0, TechSkill.PHP: 0, TechSkill.LINUX: 0, TechSkill.FIGMA: 0, TechSkill.NUMPY: 0, TechSkill.PYTORCH: 0, TechSkill.REACTJS: 0, TechSkill.VIM: 0, TechSkill.ASSEMBLY: 0, TechSkill.CSS: 0}
-        self.jobSoftSkills = {"collaborative", "team_player", "independent", "can do", "proactive", "english", "mandarin", "problem solving", "adaptive", "leadership"}
-        self.analysisJobLevel = {JobLevel.INTERN: 0, JobLevel.SENIOR: 0, JobLevel.JUNIOR: 0, JobLevel.EXECUTIVE: 0}
-        self.analysisJobType = {JobType.FULLTIME: 0, JobType.PARTTIME: 0, JobType.BOTH: 0}
-
-    def parseInput(self, input):
-        return input.lower().replace(" ", "").replace("\n", "")
-
-    def analyseJobType(self):
-        isFullTime = False
-        for keyword in JobType.FULLTIME_KEYWORDS.value:
-            if keyword in self.input:
-                isFullTime = True
-                break
+class KeywordAnalysis():
+    def __init__(self, mapping):
+        self.mapping = mapping
+        self.analysis = self._setupAnalysis()
         
-        isPartTime = False
-        for keyword in JobType.PARTTIME_KEYWORDS.value:
-            if keyword in self.input:
-                isPartTime = True
-                break
-
-        if isFullTime and isPartTime:
-            self.analysisJobType[JobType.BOTH] += 1
-            return
-        
-        if isFullTime:
-            self.analysisJobType[JobType.FULLTIME] += 1
-            return
+    def _setupAnalysis(self):
+        initial_analysis = {}
+        for key in self.mapping.value.keys():
+            initial_analysis[key] = 0
+        return initial_analysis
     
-    
-    def analyseJobLevel(self):
-        isInternship = False
-        for keyword in JobLevel.INTERNSHIP_KEYWORDS.value:
-            if keyword in self.input:
-                isInternship = True
-                break
-        
-        isJunior = False
-        for keyword in JobType.JUNIOR_KEYWORDS.value:
-            if keyword in self.input:
-                isJunior = True
-                break
+    def runAnalysis(self, input):
+        for key, keywords in self.mapping.value.items():
+            if type(keywords) == type(""):
+                if keywords in input:
+                    self.analysis[key] = 1
+                    break
+            else:
+                for keyword in keywords:
+                    if keyword in input:
+                        self.analysis[key] = 1
+                        break
 
-        isSenior = False
-        for keyword in JobType.SENIOR_KEYWORDS.value:
-            if keyword in self.input:
-                isSenior = True
-                break
+class KeywordAnalysisKeyEnum(Enum):
+    JOBTYPE = "JOB_TYPE"
+    JOBLEVEL = "JOB_LEVEL"
+    REQUIREDDEGREE = "REQUIRED_DEGREE"
+    JOBROLE = "JOB_ROLE"
+    TECHSKILL = "TECH_SKILL"
 
-        isExec = False
-        for keyword in JobType.EXECUTIVE_KEYWORDS.value:
-            if keyword in self.input:
-                isExec = True
-                break
-
-        if isInternship:
-            self.JobLevel[JobLevel.INTERN] = 1
-            return
-        
-        if isJunior:
-            self.JobLevel[JobLevel.JUNIOR] = 1
-            return
-
-        if isSenior:
-            self.JobLevel[JobLevel.SENIOR] = 1
-            return
-
-        if isExec:
-            self.JobLevel[JobLevel.EXECUTIVE] = 1
-            return
-
-
-    def analyseRequiredDegree(self):
-        isBachelor = False
-        for keyword in RequiredDegree.BACHELOR_KEYWORDS.value:
-            if keyword in self.input:
-                isBachelor = True
-                break
-
-        isMaster = False
-        for keyword in RequiredDegree.MASTER_KEYWORDS.value:
-            if keyword in self.input:
-                isMaster = True
-                break
-
-        isPhd = False
-        for keyword in RequiredDegree.PHD_KEYWORDS.value:
-            if keyword in self.input:
-                isPhd = True
-                break
-
-        isGraduate = isPhd or isMaster
-        for keyword in RequiredDegree.GRADUATE_KEYWORDS.value:
-            if keyword in self.input:
-                isGraduate = True
-                break
-
-        isNoneRequired = not (isGraduate or isBachelor)
-        for keyword in RequiredDegree.NONE_KEYWORDS.value:
-            if keyword in self.input:
-                isNoneRequired = True
-                break
-
-        if isNoneRequired:
-            self.analysisRequiredDegree[DegreeRequired.NONE_REQUIRED] = 1
-            return
-        
-        if isBachelor:
-            self.analysisRequiredDegree[DegreeRequired.BACHELOR] = 1
-
-        if isGraduate:
-            self.analysisRequiredDegree[DegreeRequired.GRADUATE] = 1
-
-        if isMaster:
-            self.analysisRequiredDegree[DegreeRequired.MASTER] = 1
-            
-        if isPhd:
-            self.analysisRequiredDegree[DegreeRequired.PHD] = 1
-            
-    def analyseJobRole(self):
-        isSE = False
-        for keyword in JobRole.SE_KEYWORDS.value:
-            if keyword in self.input:
-                isSE = True
-                break
-
-        isQA = False
-        for keyword in JobRole.QA_KEYWORDS.value:
-            if keyword in self.input:
-                isQA = True
-                break
-
-        isDS = False
-        for keyword in JobRole.DS_KEYWORDS.value:
-            if keyword in self.input:
-                isDS = True
-                break
-
-        isWebDev = False
-        for keyword in JobRole.WEBDEV_KEYWORDS.value:
-            if keyword in self.input:
-                isGraduate = True
-                break
-
-        isFrontEnd = False
-        for keyword in JobRole.FRONTEND_KEYWORDS.value:
-            if keyword in self.input:
-                isFrontEnd = True
-                break
-        
-        isBackEnd = False
-        for keyword in JobRole.BACKEND_KEYWORDS.value:
-            if keyword in self.input:
-                isBackEnd = True
-                break
-        
-        isFullstack = False
-        for keyword in JobRole.FULLSTACK_KEYWORDS.value:
-            if keyword in self.input:
-                isFullstack = True
-                break
-        
-        isResearch = False
-        for keyword in JobRole.RESEARCH_KEYWORDS.value:
-            if keyword in self.input:
-                isResearch = True
-                break
-        
-        isNetworking = False
-        for keyword in JobRole.NETWORKING_KEYWORDS.value:
-            if keyword in self.input:
-                isNetworking = True
-                break
-        
-        isTesting = False
-        for keyword in JobRole.TESTING_KEYWORDS.value:
-            if keyword in self.input:
-                isTesting = True
-                break
-        
-        isML = False
-        for keyword in JobRole.ML_KEYWORDS.value:
-            if keyword in self.input:
-                isML = True
-                break
-        
-        isAI = False
-        for keyword in JobRole.AI_KEYWORDS.value:
-            if keyword in self.input:
-                isAI = True
-                break
-        
-
-        if isSE:
-            self.analysisJobRole[JobRole.SOFTWARE_ENGINEER] = 1
-            return
-        
-        if isQA:
-            self.analysisJobRole[JobRole.QUALITY_ASSURANCE] = 1
-            return
-
-        if isDS:
-            self.analysisJobRole[JobRole.DATA_SCIENTIST] = 1
-            return
-
-        if isWebDev:
-            self.analysisJobRole[JobRole.WEB_DEVELOPER] = 1
-            return
-            
-        if isFrontEnd:
-            self.analysisJobRole[JobRole.FRONT_END] = 1
-            return
-        
-        if isBackEnd:
-            self.analysisJobRole[JobRole.BACK_END] = 1
-            return
-        
-        if isFullstack:
-            self.analysisJobRole[JobRole.FULLSTACK] = 1
-            return
-        
-        if isResearch:
-            self.analysisJobRole[JobRole.RESEARCH] = 1
-            return
-
-        if isTesting:
-            self.analysisJobRole[JobRole.TEST_ENGINEER] = 1
-            return
-
-        if isML:
-            self.analysisJobRole[JobRole.ML_ENGINEER] = 1
-            return
-
-        if isAI:
-            self.analysisJobRole[JobRole.AI_ENGINEER] = 1
-            return
-        
-        if isNetworking:
-            self.analysisJobRole[JobRole.NETWORKING] = 1
-            return
-    
-    def analyseJobRole(self):
-        isJAVA = False
-        for keyword in TechSkill.JAVA.value:
-            if keyword in self.input:
-                isJAVA = True
-                break
-
-        isJS = False
-        for keyword in TechSkill.JAVASCRIPT.value:
-            if keyword in self.input:
-                isJS = True
-                break
-
-        isCPP = False
-        for keyword in TechSkill.CPP.value:
-            if keyword in self.input:
-                isCPP = True
-                break
-
-        isPython = False
-        for keyword in TechSkill.PYTHON.value:
-            if keyword in self.input:
-                isPython = True
-                break
-
-        isSQL = False
-        for keyword in TechSkill.SQL.value:
-            if keyword in self.input:
-                isSQL = True
-                break
-
-        isRuby = False
-        for keyword in TechSkill.RUBY.value:
-            if keyword in self.input:
-                isRuby = True
-                break
-
-        isPHP = False
-        for keyword in TechSkill.PHP.value:
-            if keyword in self.input:
-                isPHP = True
-                break
-
-        isLINUX = False
-        for keyword in TechSkill.LINUX.value:
-            if keyword in self.input:
-                isLInux = True
-                break
-
-        isFigma = False
-        for keyword in TechSkill.FIGMA.value:
-            if keyword in self.input:
-                isFigma = True
-                break
-
-        isNumpy = False
-        for keyword in TechSkill.NUMPY.value:
-            if keyword in self.input:
-                isNumpy = True
-                break
-
-        isPytorch = False
-        for keyword in TechSkill.PYTORCH.value:
-            if keyword in self.input:
-                isPytorch = True
-                break
-
-        isReact = False
-        for keyword in TechSkill.REACTJS.value:
-            if keyword in self.input:
-                isReact = True
-                break
-
-        isVIM = False
-        for keyword in TechSkill.VIM.value:
-            if keyword in self.input:
-                isVIM = True
-                break
-
-        isAssembly = False
-        for keyword in TechSkill.ASSEMBLY.value:
-            if keyword in self.input:
-                isAssembly = True
-                break
-
-        isCSS = False
-        for keyword in TechSkill.CSS.value:
-            if keyword in self.input:
-                isCSS = True
-                break
-
-
-        if isJava:
-            self.analysisTechSkills[TechSkill.JAVA] = 1
-        
-        if isJS:
-            self.analysisTechSkills[TechSkill.JS] = 1
-        
-        if isCPP:
-            self.analysisTechSkills[TechSkill.CPP] = 1
-        
-        if isPython:
-            self.analysisTechSkills[TechSkill.PYTHON] = 1
-        
-        if isSQL:
-            self.analysisTechSkills[TechSkill.SQL] = 1
-        
-        if isRuby:
-            self.analysisTechSkills[TechSkill.RUBY] = 1
-        
-        if isPHP:
-            self.analysisTechSkills[TechSkill.PHP] = 1
-        
-        if isLinux:
-            self.analysisTechSkills[TechSkill.LINUX] = 1
-        
-        if isFigma:
-            self.analysisTechSkills[TechSkill.FIGMA] = 1
-        
-        if isNumpy:
-            self.analysisTechSkills[TechSkill.NUMPY] = 1
-        
-        if isPytorch:
-            self.analysisTechSkills[TechSkill.PYTORCH] = 1
-        
-        if isReact:
-            self.analysisTechSkills[TechSkill.REACTJS] = 1
-        
-        if isVIM:
-            self.analysisTechSkills[TechSkill.VIM] = 1
-        
-        if isAssembly:
-            self.analysisTechSkills[TechSkill.ASSEMBLY] = 1
-        
-        if isCSS:
-            self.analysisTechSkills[TechSkill.CSS] = 1
-        
-        
-        
-
-        
-    def print(self):
-        print(self.input)
-
-        if isPartTime:
-            self.analysisJobType[JobType.PARTTIME] += 1
-            return
-
-    def printAnalysis(self):
-        print(self.analysisJobType)
-
-class JobType(Enum):
-    FULLTIME = "FULLTIME"
-    PARTTIME = "PARTIME"
-    BOTH = "BOTH"
+class JobTypeEnum(Enum):
+    FULLTIME = "Full Time"
+    PARTTIME = "Part Time"
 
     FULLTIME_KEYWORDS = ("full time", "fulltime", "full-time")
     PARTTIME_KEYWORDS = ("part time", "parttime", "part-time")
 
-class JobLevel(Enum):
-    INTERN = "Internships"
-    SENIOR = "Senior"
+    MAPPING = {FULLTIME: FULLTIME_KEYWORDS, PARTTIME: PARTTIME_KEYWORDS}
+
+class JobLevelEnum(Enum):
+    INTERN = "Internship"
     JUNIOR = "Junior"
+    SENIOR = "Senior"
     EXECUTIVE = "Executive"
 
-
-    INTERNSHIP_KEYWORDS = ("intern", "internship")
-    SENIOR_KEYWORDS = ("senior", "sr", "experienced")
+    INTERN_KEYWORDS = ("intern")
     JUNIOR_KEYWORDS = ("junior", "jr", "entry")
+    SENIOR_KEYWORDS = ("senior", "sr", "experienced")
     EXECUTIVE_KEYWORDS = ("executive", "chief", "cto")
 
-class RequiredDegree(Enum):
+    MAPPING = {INTERN: INTERN_KEYWORDS, JUNIOR: JUNIOR_KEYWORDS, SENIOR: SENIOR_KEYWORDS, EXECUTIVE: EXECUTIVE_KEYWORDS}
+
+class RequiredDegreeEnum(Enum):
     NONE_REQUIRED = "None"
     BACHELOR = "Bachelor"
     GRADUATE = "Graduate"
@@ -433,7 +68,9 @@ class RequiredDegree(Enum):
     MASTER_KEYWORDS = ("master")
     PHD_KEYWORDS = ("phd", "doctorate", "doctorofphilosophy")
 
-class JobRole(Enum):
+    MAPPING = {NONE_REQUIRED: NONE_KEYWORDS, BACHELOR: BACHELOR_KEYWORDS, GRADUATE: GRADUATE_KEYWORDS, MASTER: MASTER_KEYWORDS, PHD: PHD_KEYWORDS}
+
+class JobRoleEnum(Enum):
     SOFTWARE_ENGINEER = "SoftEng"
     QUALITY_ASSURANCE = "QA"
     DATA_SCIENTIST = "DS"
@@ -460,8 +97,9 @@ class JobRole(Enum):
     ML_KEYWORDS = ("machinelearning", "ml")
     AI_KEYWORDS = ("artificialintelligence", "ai")
 
+    MAPPING = {SOFTWARE_ENGINEER: SE_KEYWORDS, QUALITY_ASSURANCE: QA_KEYWORDS, DATA_SCIENTIST: DS_KEYWORDS, WEB_DEVELOPER: WEBDEV_KEYWORDS, FRONT_END: FRONTEND_KEYWORDS, BACK_END: BACKEND_KEYWORDS, FULLSTACK: FULLSTACK_KEYWORDS, RESEARCH: RESEARCH_KEYWORDS, NETWORKING: NETWORKING_KEYWORDS, TEST_ENGINEER: TESTING_KEYWORDS, ML_ENGINEER: ML_KEYWORDS, AI_ENGINEER: AI_KEYWORDS}
 
-class TechSkill(Enum):
+class TechSkillEnum(Enum):
     JAVA = "Java"
     JAVASCRIPT = "JS"
     CPP = "CPP"
@@ -494,6 +132,36 @@ class TechSkill(Enum):
     ASSEMBLY_KEYWORDS = ("assembly")
     CSS_KEYWORDS = ("css")
 
+    MAPPING = {JAVA: JAVA_KEYWORDS, JAVASCRIPT: JS_KEYWORDS, CPP: CPP_KEYWORDS, PYTHON: PYTHON_KEYWORDS, SQL: SQL_KEYWORDS, RUBY: RUBY_KEYWORDS, PHP: PHP_KEYWORDS, LINUX: LINUX_KEYWORDS, FIGMA: FIGMA_KEYWORDS, NUMPY: NUMPY_KEYWORDS, PYTORCH: PYTHON_KEYWORDS, REACTJS: REACT_KEYWORDS, VIM: VIM_KEYWORDS, ASSEMBLY: ASSEMBLY_KEYWORDS, CSS: CSS_KEYWORDS}
+
+class Analysis():
+    def __init__(self, input):
+        self.input = self.parseInput(input)
+        self.keywordAnalysis = self._runKeywordAnalysis()
+
+    def parseInput(self, input):
+        return input.lower().replace("\n", " ")
+
+    def _runKeywordAnalysis(self):
+        result = {}
+
+        mapping = {}
+        mapping[KeywordAnalysisKeyEnum.JOBTYPE] = KeywordAnalysis(JobTypeEnum.MAPPING)
+        mapping[KeywordAnalysisKeyEnum.JOBLEVEL] = KeywordAnalysis(JobLevelEnum.MAPPING)
+        mapping[KeywordAnalysisKeyEnum.REQUIREDDEGREE] = KeywordAnalysis(RequiredDegreeEnum.MAPPING)
+        mapping[KeywordAnalysisKeyEnum.JOBROLE] = KeywordAnalysis(JobRoleEnum.MAPPING)
+        mapping[KeywordAnalysisKeyEnum.TECHSKILL] = KeywordAnalysis(TechSkillEnum.MAPPING)
+
+        for key, value in mapping.items():
+            value.runAnalysis(self.input)
+            result[key.value] = value.analysis
+
+        return result
+    
+    def printKeywordAnalysis(self):
+        print(self.keywordAnalysis)
+
+
 ### TEST ###
 
 text = Get("https://jobs.polymer.co/whalesync/28574")
@@ -503,8 +171,4 @@ soup = htmlParser.GetSoup()
 text = soup.get_text()
 
 analysis = Analysis(text)
-# print(analysis.input)
-
-analysis.analyseJobType()
-analysis.printAnalysis()
-
+analysis.printKeywordAnalysis()
