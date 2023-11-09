a1cb22a8244bbefd17636d2c4bb6205a58b6061d
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2023-11-09 21:27:56 +0800

    add comments

--- Webcrawler.py
+++ Webcrawler.py
@@ -13,10 +13,14 @@ import time
 
 class Webcrawler:
     def crawl(self, url: str) -> Optional[TaskResult]:
+        """
+        crawl crawls the given url and returns a TaskResult.
+        """
         print("crawling: ", url)
 
         html = ""
         try:
+            # record time
             start_time = time.time()
             html = Get(url)
             end_time = time.time()
@@ -24,21 +28,27 @@ class Webcrawler:
             print("Error in Get:", e)
             return None
 
+        # parse html response
         parser = HTMLParser(html)
         next_urls = parser.GetLinks()
 
+        # analyze html response
         soup = parser.GetSoup()
         analysis = Analysis(soup.get_text())
         ip_addr = self.get_ip_addr(url)
         address_info = DbIpCity.get(ip_addr, api_key="free")
+
         return TaskResult(
             url=url,
             ip_addr=address_info.ip_address,
             geolocation=f"{address_info.city}, {address_info.region}, {address_info.country}",
             next_urls=next_urls,
-            rtt=round(end_time-start_time, 3),
-            analysis=analysis
+            rtt=round(end_time - start_time, 3),
+            analysis=analysis,
         )
 
     def get_ip_addr(self, url: str) -> str:
+        """
+        get_ip_addr returns the IP address for the given url.
+        """
         return socket.gethostbyname(parse.urlparse(url).hostname)
