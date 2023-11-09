3770af33d33d888709b9a4d6a5cc4db286c51240
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2023-11-09 21:08:32 +0800

    chore: add comments for HTMLParser.py

--- HTMLParser.py
+++ HTMLParser.py
@@ -3,14 +3,32 @@ from bs4 import BeautifulSoup
 
 class HTMLParser():
     def __init__(self, HTML):
+        """
+        Initialize the HTMLParser class with the provided HTML content.
+        
+        Args:
+            HTML (str): The HTML content to be parsed.
+        """
         self.HTML = HTML
         self.Soup = self._createSoup()
 
     def _createSoup(self):
+        """
+        Private method to create a BeautifulSoup object from the HTML content.
+
+        Returns:
+            BeautifulSoup: A BeautifulSoup object for parsing the HTML content.
+        """
         soup = BeautifulSoup(self.HTML, 'html.parser')
         return soup
     
     def GetLinks(self):
+        """
+        Extract and return HTTP links (http:// and https://) from the HTML content.
+
+        Returns:
+            list: A list of HTTP links found in the HTML content.
+        """
         links = self.Soup.find_all('a')
 
         result = []
@@ -23,10 +41,19 @@ class HTMLParser():
         return result
     
     def GetHTML(self):
+        """
+        Get the original HTML content.
+
+        Returns:
+            str: The original HTML content.
+        """
         return self.HTML
     
     def GetSoup(self):
-        return self.Soup
+        """
+        Get the BeautifulSoup object for parsed HTML.
 
-# htmlParser = HTMLParser("www.example.com")
-# print(htmlParser.GetLinks())
+        Returns:
+            BeautifulSoup: A BeautifulSoup object for parsing the HTML content.
+        """
+        return self.Soup
